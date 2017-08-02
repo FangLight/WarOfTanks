e476d5531a5e419ff71798bfdd042431ae2f1e33
Fang Lai <fang.lai415@gmail.com>
2017-08-02 00:38:27 -0600

    First commit, project start!

---
+++ src/dev/Game.java
@@ -0,0 +1,11 @@
+package dev;
+
+public class Game {
+    Game(){
+        System.out.println("Game init!");
+    }
+
+    public void start(){
+        System.out.println("Game start!");
+    }
+}
---
+++ src/dev/Launcher.java
@@ -0,0 +1,10 @@
+package dev;
+
+public class Launcher {
+
+    public static void main(String[] args){
+        Game game = new Game();
+        game.start();
+    }
+
+}
